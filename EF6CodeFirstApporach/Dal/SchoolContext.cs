eccf7f3255e1515e2c533808faea487652f31a8a
Ajay Nallanagula <Ajay_Nallanagula@epam.com>
2017-10-05 16:56:59 +0530

    Ignored Items , Added EFCore Functionality

---
+++ EF6CodeFirstApporach/Dal/SchoolContext.cs
@@ -0,0 +1,30 @@
+﻿using EF6CodeFirstApporach.Models;
+using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Conventions;
+
+namespace EF6CodeFirstApporach.Dal
+{
+    public class SchoolContext : DbContext
+    {
+        public DbSet<Student> Students { get; set; }
+        //below two properties are optional to mention because Student class already have a reference to Enrollment, and Enrollment have areference to Course, The Entity Framework would include them implicitly
+        public DbSet<Enrollment> Enrollments { get; set; } // Can be usefulf If you are using Seed method during development
+        public DbSet<Course> Courses { get; set; } // Can be usefulf If you are using Seed method during development
+
+        public SchoolContext() : base("SchoolConStr") //If Connection string name is not mentioned , Entity Framework assumes that the connection string name is the same as the class name in this case SchoolContext, optionally you can give the connection string with Databasename , EF will create the same database name
+        {
+
+        }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            //If you didn't do this, the generated tables in the database would be named Students, Courses, and Enrollments. Now after removing Pluralizing the name of Tables would be Student,Enrollmet,Cours
+            modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
+            modelBuilder.Entity<Enrollment>().ToTable("Enrollments"); //This will Pluralize only Enrollments Table 
+        }
+    }
+}
+
+/*
+        1) DbContext corresponds to your database (or a collection of tables and views in your database) whereas a DbSet corresponds to a table or view in your database.
+*/
--- EF6CodeFirstApporach/Models/Enrollment.cs
+++ EF6CodeFirstApporach/Models/Enrollment.cs
@@ -6,13 +6,13 @@
         public int CourseID { get; set; }
         public int StudentID { get; set; }
         public Grade? Grade { get; set; }
-
         public virtual Course Course { get; set; }
         public virtual Student Student { get; set; }
     }
 
-   public enum Grade
+    public enum Grade
     {
-        A,B,C,D,F
+        A, B, C, D, F
     }
+
 }
