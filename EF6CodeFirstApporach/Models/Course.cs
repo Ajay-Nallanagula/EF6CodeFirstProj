080b1f1760e9519205b6a583fbe4ecbeea0094da
Ajay-Nallanagula <ajay_nallanagula@epam.com>
2017-10-05 13:39:26 +0530

    Solution thats is self sufficent to build

---
+++ EF6CodeFirstApporach/Models/Course.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace EF6CodeFirstApporach.Models
+{
+    public class Course
+    {
+        [DatabaseGenerated(DatabaseGeneratedOption.None)]
+        public int CourseID { get; set; } //this property will not be generated  by db, we have to generate it manually
+        public string Title { get; set; }
+        public int Credits { get; set; }
+
+        public virtual ICollection<Enrollment> Enrollments { get; set; }
+    }
+}
---
+++ EF6CodeFirstApporach/Models/Student.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace EF6CodeFirstApporach.Models
+{
+    public class Student
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+        public DateTime EnrollmentDate { get; set; }
+
+        //Property is virtual because it helps in lazy loading
+        public virtual ICollection<Enrollment> Enrollments { get; set; } //Navigation Property
+
+    }
+}
