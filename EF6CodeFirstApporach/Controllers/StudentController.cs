ae2e28eb684a59dc679a1071cb14e4f73796d22a
Ajay Nallanagula <Ajay_Nallanagula@epam.com>
2017-10-06 17:19:56 +0530

    Few more changes added

--- EF6CodeFirstApporach/Controllers/StudentController.cs
+++ EF6CodeFirstApporach/Controllers/StudentController.cs
@@ -48,7 +48,7 @@ namespace EF6CodeFirstApporach.Controllers
         // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Create([Bind(Include = "Id,FirstName,LastName,EnrollmentDate")] Student student)
+        public ActionResult Create([Bind(Include = "Id,FirstName,LastName,EnrollmentDate,EmailAddress")] Student student)
         {
             try
             {
@@ -59,10 +59,10 @@ namespace EF6CodeFirstApporach.Controllers
                     return RedirectToAction("Index");
                 }
             }
-            catch(DataException ex)
+            catch (DataException ex)
             {
                 Trace.WriteLine(ex.Message);
-                ModelState.AddModelError("UNABLE TO SAVE:","Unable to save the data at this moment, Contact your system Administrator");
+                ModelState.AddModelError("UNABLE TO SAVE:", "Unable to save the data at this moment, Contact your system Administrator");
             }
 
             return View(student);
@@ -86,25 +86,43 @@ namespace EF6CodeFirstApporach.Controllers
         // POST: Student/Edit/5
         // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
         // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
-        [HttpPost]
+        [HttpPost, ActionName("Edit")]
         [ValidateAntiForgeryToken]
-        public ActionResult Edit([Bind(Include = "Id,FirstName,LastName,EnrollmentDate")] Student student)
+        public ActionResult EditPost(int? id)
         {
-            if (ModelState.IsValid)
+            if (id == null)
             {
-                db.Entry(student).State = EntityState.Modified;
-                db.SaveChanges();
-                return RedirectToAction("Index");
+                return new HttpStatusCodeResult(HttpStatusCode.NotFound);
+            }
+
+            Student student = db.Students.Find(id);
+            bool isUpdated = TryUpdateModel(student, new string[] { "FirstName", "LastName", "EnrollmentDate", "EmailAddress" });
+            try
+            {
+                if (isUpdated)
+                {
+                    db.SaveChanges();
+                    return RedirectToAction("Index");
+                }
+            }
+            catch (Exception ex)
+            {
+                Trace.WriteLine(ex.Message);
+                ModelState.AddModelError("EditError", "Unable to edit the record this time, Contact SystemAdminstrator");
             }
             return View(student);
         }
 
         // GET: Student/Delete/5
-        public ActionResult Delete(int? id)
+        public ActionResult Delete(int? id,bool saveChagesError=false)
         {
             if (id == null)
             {
                 return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            if (saveChagesError)
+            {
+                ViewBag.ErrorMessage = "Oops delete operation gone wrong , contact sysadmin";
             }
             Student student = db.Students.Find(id);
             if (student == null)
@@ -119,9 +137,22 @@ namespace EF6CodeFirstApporach.Controllers
         [ValidateAntiForgeryToken]
         public ActionResult DeleteConfirmed(int id)
         {
-            Student student = db.Students.Find(id);
-            db.Students.Remove(student);
-            db.SaveChanges();
+            try
+            {
+                //This will sent an extra query to database i.e find/select query 
+                // Student student = db.Students.Find(id);
+                // db.Students.Remove(student);
+
+                //As an alternative to above code we can set theEntityState per record to Deleteas shown below 
+                Student studentToDelete = new Student() { Id = id };
+                db.Entry(studentToDelete).State = EntityState.Deleted;
+                db.SaveChanges();
+            }
+            catch(DataException ex)
+            {
+                Trace.WriteLine(ex.Message);
+                RedirectToAction("Delete", new { Id = id, saveChagesError = true });
+            }
             return RedirectToAction("Index");
         }
 
